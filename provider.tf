a066aabfd7c5c57038cbccc197446b6c72431156
Vijaygvasan121 <134586324+Vijaygvasan121@users.noreply.github.com>
2024-05-21 16:39:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,4 @@
-terraform {
-  required_version = "> 0.14"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
+
 
 # Provider Block
 provider "aws" {
